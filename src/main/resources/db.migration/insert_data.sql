0d812e508a4658fe157a0a8b2cc9d09f8573f557
Nikita-2201 <ma0skoko03@gmail.com>
2024-03-12 22:39:17 +0700

    add sql req

---
+++ src/main/resources/db.migration/insert_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO computer (color, size, price, category, processor_type, tech_entity_id)
+VALUES ('Black', 'Desktop', 1499.99, 'Gaming', 'AMD Ryzen 9', 1),
+       ('Blue', 'Laptop', 1499.99, 'Gaming', 'Intel i7', 1),
+       ('Blue', 'Desktop', 1499.99, 'WorkPC', 'AMD Ryzen 9', 2);
+
+INSERT INTO refrigerator (color, size, price, number_of_doors, compressor_type, tech_entity_id)
+VALUES ('Black', 'French Door', 1499.99, 2, 'Inverter', 3),
+       ('Blue', 'French Door', 1499.99, 2, 'Inverter', 3),
+       ('Blue', 'French Door', 1499.99, 3, 'Inverter', 4);
+
+INSERT INTO smartphone (color, memory, number_of_cameras, price, size, tech_entity_id)
+VALUES ('Black', 256, 3, 899.99, '6.1 inches', 5),
+       ('Green', 128, 2, 600.50, '6.1 inches', 5),
+       ('Blue', 64, 1, 500, '6.1 inches', 6);
+
+INSERT INTO television (category, color, price, size, technology, tech_entity_id)
+VALUES ('Smart TV', 'Silver', 1499.99, '65 inches', 'QLED', 7),
+       ('Smart TV', 'Silver', 1300.99, '55 inches', 'OLED', 7),
+       ('TV', 'Silver', 1000, '45 inches', 'IPS', 8);
+
+INSERT INTO vacuum_cleaner (color, dust_bag_volume, modes, price, size, tech_entity_id)
+VALUES ('White', 3.5, 3, 700, 'Compact', 2),
+       ('White', 2.5, 3, 600, 'Compact', 4),
+       ('Black', 2, 2, 500, 'Compact', 6),
+       ('Black', 2.3, 1, 300, 'Compact', 8);
+
+
+
+
---
+++ src/main/resources/db.migration/insert_into_tech_entity.sql
@@ -0,0 +1,10 @@
+INSERT INTO tech_entity (available, installment_available, manufacturer_company, manufacturer_country, name,
+                         online_order_available, serial_number)
+VALUES (true, false, 'Example Company1', 'Example Country1', 'Example Product1', true, '1'),
+       (true, false, 'Example Company2', 'Example Country2', 'Example Product2', true, '12'),
+       (true, false, 'Example Company3', 'Example Country3', 'Example Product3', true, '123'),
+       (true, false, 'Example Company4', 'Example Country4', 'Example Product4', true, '1234'),
+       (true, false, 'Example Company5', 'Example Country5', 'Example Product5', true, '12345'),
+       (true, false, 'Example Company6', 'Example Country6', 'Example Product6', true, '123456'),
+       (true, false, 'Example Company7', 'Example Country7', 'Example Product7', true, '1234567'),
+       (true, false, 'Example Company8', 'Example Country8', 'Example Product8', true, '12345678');
